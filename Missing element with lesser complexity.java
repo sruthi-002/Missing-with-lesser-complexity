a2b9c294c71fd5f76d3f894d7720eeed9e9d60f0
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-01 13:40:36 +0530

    Add files via upload

---
+++ Missing element with lesser complexity.java
@@ -0,0 +1,38 @@
+﻿package com.company;
+import java.util.Scanner;
+import java.util.*;
+class missing
+{
+    public void  missingelement()
+    {
+        int count=0;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i,j,miss=0;
+        ArrayList<Integer>a = new ArrayList<Integer>();
+        for(i=0;i<n;i++)
+            a.add(in.nextInt());
+        Collections.sort(a);
+        for(i=1;i<n;i++)
+        {
+            if(a.get(i-1)==a.get(i))
+                count++;
+            if(a.contains(i+1))
+                miss++;
+            j =i+1;
+            if (miss == 0) {
+                System.out.println("The missing element is " + " " +j);
+            }
+            if(count>=1)
+                System.out.println("The Duplicate element is "+" "+a.get(i));
+            count=0;
+            miss=0;
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        missing m =new missing();
+        m.missingelement();
+    }
+}
